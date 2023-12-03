7e5edf92b21fd0bae8105b383372cd27fd21b39b
Gabriel Zerio <21043296+gabrielzerio@users.noreply.github.com>
2023-12-03 18:50:39 -0300

    projeto adicionado

---
+++ src/Quarto.java
@@ -0,0 +1,32 @@
+public class Quarto {
+
+    private int numero;
+    private boolean reservado;
+    private int capacidade;
+
+    public Quarto(int numero, int capacidade) {
+        this.numero = numero;
+        this.capacidade = capacidade;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public boolean getReservado() {
+        return reservado;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setReservado(boolean reservado) {
+        this.reservado = reservado;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+}
